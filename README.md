dfa2d4a8184bd8a319ccec878b413d0b630bcdc3
vatsa9 <vatsa9myself@gmail.com>
2018-06-20 16:48:57 -0400

    Code Commit

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# Intel_Simulator
+
+This is a demonstration of collision-detection and driving using a feedforward neural network from Interactive Robotics Lab at Arizona State University.
+
+
+## Requirements
+
+Please install Pygame and PyMunk in Python3 environment.
+
+```python
+pip install pygame
+pip install pymunk
+```
+
+## Usage
+
+To run this project. Please do the following steps
+
+For collecting the collision data for training:
+```python
+python training_the_model.py
+```
+
+For training the neural network:
+```python
+python make_it_learn.py
+```
+
+For running the project which learned to take right actions avoiding collisions:
+```python
+python playing_the_model.py
+```
+Car learns to avoid collision but by default it moves to towards the destination.
+
+
+
+## Feedback
+
+Questions or comments may be directed to Nambi Srivatsav at <nsrivats@asu.edu>
---
+++ make_it_learn.py
@@ -0,0 +1,178 @@
+import torch
+import torch.nn as nn
+import torchvision.datasets as dsets
+import torchvision.transforms as transforms
+from torch.autograd import Variable
+import pdb
+import numpy as np
+
+torch.manual_seed(10)
+
+# Hyper Parameters 
+input_size = 6
+hidden_size = 200
+num_classes = 1
+num_epochs = 600
+batch_size = 1
+learning_rate = 0.001
+
+
+# Neural Network Model (1 hidden layer)
+class Net(nn.Module):
+    def __init__(self, input_size, hidden_size, num_classes):
+        super(Net, self).__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size) 
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(hidden_size, hidden_size) 
+        self.fc3 = nn.Linear(hidden_size, num_classes)
+  
+        
+    def forward(self, x):
+        
+        out = self.fc1(x)
+        out = self.relu(out)
+        out = self.fc2(out)
+        out = self.relu(out)
+        out = self.fc3(out)
+        return out
+
+
+if __name__ == "__main__":
+        
+    net = Net(input_size, hidden_size, num_classes)
+
+        
+    # Loss and Optimizer
+    criterion = nn.MSELoss()  
+    optimizer = torch.optim.Adam(net.parameters(), lr=learning_rate)  
+
+    sensor_data = np.loadtxt('./sensor_data/sensor_data.txt')
+    sensor_data[:,-2] = (sensor_data[:,-2] + 40)/460
+    sensor_data[:,:-2] = (sensor_data[:,:-2] +1)/11
+    
+   
+    #sensor_data_last_row_2 = np.roll(sensor_data[:,-1], -2) # how many timesteps before you want to find out collision
+    sensor_data_row_0 = sensor_data[:,-1]
+    sensor_data_row_1 = np.roll(sensor_data[:,-1], -1) # how many timesteps before you want to find out collision
+    sensor_data_row_2 = np.roll(sensor_data[:,-1], -2) # how many timesteps before you want to find out collision
+    sensor_data_row_3 = np.roll(sensor_data[:,-1], -3)
+    sensor_data_row_4 = np.roll(sensor_data[:,-1], -4)
+    
+    sensor_data[:,-1] = sensor_data_row_0 + sensor_data_row_1 + sensor_data_row_2 + sensor_data_row_3 + sensor_data_row_4
+    
+    np.savetxt('./sensor_data/labeled_sensor_data.txt',sensor_data)
+    
+    
+    
+    collision_full_data = sensor_data[ sensor_data[:,-1] > 0 ]
+
+    
+    
+    # Duplicating collision Data for faster learning
+    
+    for i in range(6):
+        sensor_data = np.append(sensor_data,collision_full_data,axis=0)
+    
+    np.random.shuffle(sensor_data)
+    
+    
+    
+    sensor_nn_data = torch.Tensor(sensor_data[:,:-1])
+    collision_data = torch.Tensor(collision_full_data[:,:-1])
+
+    sensor_nn_labels = torch.Tensor(sensor_data[:,-1]).view(-1,1)
+    collision_sensor_nn_labels = torch.Tensor(collision_full_data[:,-1]).view(-1,1)
+    
+    total = sensor_nn_data.shape[0]
+    train_size = int(0.70*total)
+    test_size = total - train_size
+    train_sensor_nn_data = sensor_nn_data[:train_size]
+    train_sensor_nn_labels = sensor_nn_labels[:train_size]
+    
+    
+    for j in range(num_epochs):
+
+        losses = 0
+        for i in range(train_size):  
+            # Convert torch tensor to Variable
+            input_values = Variable(sensor_nn_data[i])
+            labels = Variable(sensor_nn_labels[i])
+            
+            # Forward + Backward + Optimize
+            optimizer.zero_grad()  # zero the gradient buffer
+            outputs = net(input_values)
+            
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            losses += loss.data[0]
+            
+        print ('Epoch %d, Loss: %.4f' %(j+1, losses/sensor_nn_data.shape[0]))
+        
+        if(j%25 == 0):
+            
+            imgs = sensor_nn_data
+            lbls = sensor_nn_labels
+
+            correct = 0
+            total = imgs.shape[0]
+                
+            for i in range(train_size,total):  
+                
+                input_values = Variable(imgs[i])
+                labels = Variable(lbls[i])
+                
+                outputs = net(input_values)
+                predicted = torch.round(outputs.data)[0]
+                
+                #correct += (predicted == labels).data[0]
+                
+                labels = labels.data[0]
+                predicted = predicted
+                
+                if(labels == 0 and predicted == 0):
+                    correct += 1
+                
+                if(labels >= 1 and predicted >= 1):
+                    correct += 1
+            
+            print('Accuracy of the network on the  test input_values: %d %%' % (100 * correct / test_size), correct, test_size)
+        
+        torch.save(net.state_dict(), './saved_nets/nn_car_model.pkl')
+        
+        
+        
+        if(j%25 == 0):
+
+            correct = 0
+            total = collision_data.shape[0]
+            
+            for i in range(total):  
+                
+                input_values = Variable(collision_data[i])
+                labels = Variable(collision_sensor_nn_labels[i])
+                
+                
+                outputs = net(input_values)
+                predicted = torch.round(outputs.data)[0]
+                
+                labels = labels.data[0]
+                predicted = predicted
+                
+                if(labels == 0 and predicted == 0):
+                    correct += 1
+                
+                elif(labels == predicted):
+                    correct += 1
+                else:
+                    #print(input_values.data.numpy(),labels,predicted)
+                    print("")
+                    
+            
+            print('Accuracy of the network on the test input_values: %d %%' % (100 * correct / total), correct, total)
+            
+            torch.save(net.state_dict(), './saved_nets/nn_car_model.pkl')
+           
+        
+
+
